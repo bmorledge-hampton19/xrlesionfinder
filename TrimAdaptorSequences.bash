c66a396ebaad0b933ac822a995038f2db5a45504
Benjamin Morledge-Hampton <brotherbenben@yahoo.com>
2022-06-01 16:32:51 -0700

    Created scripts for simple adaptor trimming

---
+++ TrimAdaptorSequences.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script takes a gzipped fastq file and a file of adapater sequences to trim and uses them to run trimmomatic
+# and trim the adaptor sequences off of the reads.
+
+# Get trimmomatic's jar path.
+trimmomaticPath=$(dpkg -L trimmomatic | grep .jar$ | head -1)
+
+# Get the main directory that data is being stored in.
+dataDirectory=${1%/*}
+
+echo
+fastqFile=$1; shift
+adaptorFile=$1; shift
+
+# Ensure the input is a gzipped fastq file.
+if [[ $inputData == *\.fastq\.gz ]]
+    # If given a gzipped fastq file, set the dataName and rawFastq variables accordingly.
+    dataName=${fastqFile%.fastq.gz}
+
+else
+    echo "Error: given file: $fastqFile is not a gzipped fastq file."
+    exit 1
+
+fi
+
+echo "Working with $fastqFile"
+
+# Create the name of the trimmed output file.
+trimmedFastq="${dataName}_trimmed.fastq.gz"
+
+# Trim the data (Single End)
+echo "Trimming adaptors..."
+java -jar $trimmomaticPath SE $fastqFile $trimmedFastq "ILLUMINACLIP:$adaptorFile:2:30:10"
---
+++ TrimAdaptorSequences.py
@@ -0,0 +1,25 @@
+import os, subprocess, time
+from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog
+from mutperiodpy.helper_scripts.UsefulFileSystemFunctions import getDataDirectory
+
+
+# For each of the given reads files, run the accompyaning bash script to perform the alignment.
+def trimAdaptorSequences(fastqFilePaths, adaptorSequencesFilePath):
+    
+    trimmingBashScriptFilePath = os.path.join(os.path.dirname(__file__),"TrimAdaptor.bash")
+
+    for fastqFilePath in fastqFilePaths:
+        subprocess.run(("bash", trimmingBashScriptFilePath, fastqFilePath, adaptorSequencesFilePath), check = True)
+
+
+def main():
+
+    with TkinterDialog(workingDirectory=getDataDirectory()) as dialog:
+        dialog.createMultipleFileSelector("Raw fastq reads:", 0, ".fastq.gz", 
+                                          ("Gzipped fastq Files", ".fastq.gz"))
+        dialog.createFileSelector("Adaptor Sequences:", 1, ("Fasta Files", ".fa"))
+
+    trimAdaptorSequences(dialog.selections.getFilePathGroups()[0], dialog.selections.getIndividualFilePaths()[0])
+
+
+if __name__ == "__main__": main()
